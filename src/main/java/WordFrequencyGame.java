883c7a3ed212729ab9946f5936373532b60582ee
Alex <mingzhangliu@gmail.com>
2020-11-30 17:11:11 +0800

    [WordFrequencyGame][Alex Liu] refactor: removed duplicate code

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -2,37 +2,34 @@ import java.util.*;
 
 public class WordFrequencyGame {
     public String getResult(String inputSentence) {
-        if (inputSentence.split("\\s+").length == 1) {
-            return inputSentence + " 1";
-        } else {
-            try {
-                //split the input string with 1 to n pieces of spaces
-                String[] arrayWord = inputSentence.split("\\s+");
-                List<Input> inputList = new ArrayList<>();
-                for (String word : arrayWord) {
-                    Input input = new Input(word, 1);
-                    inputList.add(input);
-                }
-                //get the map for the next step of sizing the same word
-                Map<String, List<Input>> mapWordCount = getListMap(inputList);
-                List<Input> listWordCount = new ArrayList<>();
-                for (Map.Entry<String, List<Input>> entry : mapWordCount.entrySet()) {
-                    Input input = new Input(entry.getKey(), entry.getValue().size());
-                    listWordCount.add(input);
-                }
-                inputList = listWordCount;
-                inputList.sort((word1, word2) -> word2.getWordCount() - word1.getWordCount());
-                StringJoiner joiner = new StringJoiner("\n");
-                for (Input word : inputList) {
-                    String wordCountLine = word.getValue() + " " + word.getWordCount();
-                    joiner.add(wordCountLine);
-                }
-                return joiner.toString();
-            } catch (Exception calculateErrorException) {
-                return "Calculate Error";
+        try {
+            //split the input string with 1 to n pieces of spaces
+            String[] arrayWord = inputSentence.split("\\s+");
+            List<Input> inputList = new ArrayList<>();
+            for (String word : arrayWord) {
+                Input input = new Input(word, 1);
+                inputList.add(input);
             }
+            //get the map for the next step of sizing the same word
+            Map<String, List<Input>> mapWordCount = getListMap(inputList);
+            List<Input> listWordCount = new ArrayList<>();
+            for (Map.Entry<String, List<Input>> entry : mapWordCount.entrySet()) {
+                Input input = new Input(entry.getKey(), entry.getValue().size());
+                listWordCount.add(input);
+            }
+            inputList = listWordCount;
+            inputList.sort((word1, word2) -> word2.getWordCount() - word1.getWordCount());
+            StringJoiner joiner = new StringJoiner("\n");
+            for (Input word : inputList) {
+                String wordCountLine = word.getValue() + " " + word.getWordCount();
+                joiner.add(wordCountLine);
+            }
+            return joiner.toString();
+        } catch (Exception calculateErrorException) {
+            return "Calculate Error";
         }
     }
+
 
     private Map<String, List<Input>> getListMap(List<Input> inputList) {
         Map<String, List<Input>> mapWordCount = new HashMap<>();
