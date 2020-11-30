d0a1ae87f0fa44ec6cc5068e1bc581ef14752d55
Alex <mingzhangliu@gmail.com>
2020-11-30 17:19:17 +0800

    [WordFrequencyGame][Alex Liu] refactor: renamed variable

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -6,18 +6,16 @@ public class WordFrequencyGame {
     private final String calculateErrorMessage = "Calculate Error";
     public String getResult(String inputSentence) {
         try {
-            //split the input string with 1 to n pieces of spaces
             String[] arrayWord = inputSentence.split(REGEX_WHITESPACE);
             List<Input> inputList = new ArrayList<>();
             for (String word : arrayWord) {
                 Input input = new Input(word, 1);
                 inputList.add(input);
             }
-            //get the map for the next step of sizing the same word
             Map<String, List<Input>> mapWordCount = getListMap(inputList);
             List<Input> listWordCount = new ArrayList<>();
-            for (Map.Entry<String, List<Input>> entry : mapWordCount.entrySet()) {
-                Input input = new Input(entry.getKey(), entry.getValue().size());
+            for (Map.Entry<String, List<Input>> mapWordCountEntry : mapWordCount.entrySet()) {
+                Input input = new Input(mapWordCountEntry.getKey(), mapWordCountEntry.getValue().size());
                 listWordCount.add(input);
             }
             inputList = listWordCount;
@@ -36,7 +34,6 @@ public class WordFrequencyGame {
     private Map<String, List<Input>> getListMap(List<Input> inputList) {
         Map<String, List<Input>> mapWordCount = new HashMap<>();
         for (Input input : inputList) {
-//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
             if (!mapWordCount.containsKey(input.getValue())) {
                 ArrayList listWordCount = new ArrayList<>();
                 listWordCount.add(input);
